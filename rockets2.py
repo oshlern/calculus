5544fead0ce64f04f25792ae8079165850f7906a
laumitt <laumitt@nuevaschool.org>
2018-05-22 08:59:25 -0700

    fixed and labeled graphs

--- rockets2.py
+++ rockets2.py
@@ -56,78 +56,85 @@ def check_inputs(rocket_mass_total, fuel_total, have_error, PAYLOAD):
 
 def calculate(rocket_mass_total, fuel_total, eng_num, eng_vel, PAYLOAD, vel_burnout_total, stages_list, current_list, next_list, E_k_list, P_k_list, vel_list):
     eng_mass_fuel = fuel_total/eng_num # find mass of fuel per engine
-    print("eng_mass_fuel " + str(eng_mass_fuel))
+    print("Fuel engine mass: " + str(eng_mass_fuel) + " Kg.")
     eng_mass_struct = (rocket_mass_total - PAYLOAD) - eng_mass_fuel/eng_num # find mass of engine without fuel
-    print("eng_mass_struct " + str(eng_mass_struct))
+    print("Structural engine mass: " + str(eng_mass_struct) + " Kg.")
     stages_left = eng_num # each engine is a stage
-    print("stages_left " + str(stages_left))
+    print(str(stages_left) + " stages left.")
     vel_burnout_temp = [] # list to store all velocities independently
     vel_burnout_neg = 0
     i = 0
     for n in range(eng_num): # jen's suggestion for changing engine size: for size in [list of things that can change]
         stages_list.append(stages_left)
         current_stage_mass = (eng_mass_struct + eng_mass_fuel) * (stages_left) + PAYLOAD
-        print("current_stage_mass " + str(current_stage_mass))
+        print("Mass of current stage: " + str(current_stage_mass) + " Kg.")
         next_stage_mass = (eng_mass_struct + eng_mass_fuel) * (stages_left - 1) + PAYLOAD
-        print("next_stage_mass " + str(next_stage_mass))
+        print("Mass of next stage: " + str(next_stage_mass) + " Kg.")
         current_list.append(current_stage_mass)
         next_list.append(next_stage_mass)
         E_k = ((eng_mass_struct)/(eng_mass_struct + eng_mass_fuel))
-        print("E_k " + str(E_k))
+        print("Structural ratio: " + str(E_k) + ".")
         E_k_list.append(E_k)
         P_k = ((next_stage_mass)/(current_stage_mass))
-        print("P_k " + str(P_k))
+        print("Payload ratio: " + str(P_k) + ".")
         P_k_list.append(P_k)
         vel_burnout_eng = -eng_vel * math.log((E_k + ((1 - E_k) * P_k)))
-        print("vel_burnout_eng " + str(vel_burnout_eng))
+        print("Single engine velocity at burnout: " + str(vel_burnout_eng) + ".")
         vel_burnout_temp.append(vel_burnout_eng)
         vel_list.append(vel_burnout_eng)
         stages_left -= 1
-        print("stages_left " + str(stages_left))
+        print(str(stages_left) + "stages left.")
     for x in vel_burnout_temp:
         vel_burnout_total += (vel_burnout_temp[i])
         i += 1
-    print("vel_burnout_total " + str(vel_burnout_total))
+    print("Total velocity at burnout: " + str(vel_burnout_total) + " Km/s.")
     return vel_burnout_total, stages_list, current_list, next_list, E_k_list, P_k_list, vel_list
 
 def plot_values(stages_list, current_list, next_list, E_k_list, P_k_list, vel_list, eng_num):
     plt.figure(1)
-    plt.plot(stages_list, current_list, 'b-', stages_list, next_list, 'r-')
-    plt.xlabel('number of stages')
-    plt.ylabel('mass in Kg')
-    print("figure 1 is current stage mass (blue) and next stage mass (red) by stage")
+    plt.title("Current and Next Stage Mass by Stage")
+    plt.plot(stages_list, current_list, 'b-', label = "Current Stage Mass")
+    plt.plot(stages_list, next_list, 'r-', label = "Next Stage Mass")
+    plt.legend(loc = 'upper right')
+    plt.xlabel('Stages Left')
+    plt.ylabel('Mass in Kg')
+    plt.gca().invert_xaxis()
     plt.figure(2)
-    plt.plot(stages_list, E_k_list, 'g-', stages_list, P_k_list, 'k-')
-    plt.xlabel('number of stages')
-    plt.ylabel('ratio')
-    print("figure 2 is structural ratio (green) and payload ratio (black) by stage")
+    plt.title("Structural and Payload Ratio by Stage")
+    plt.plot(stages_list, E_k_list, 'g-', label = "Structural Ratio")
+    plt.plot(stages_list, P_k_list, 'k-', label = "Payload Ratio")
+    plt.legend(loc = 'center right')
+    plt.xlabel('Stages Left')
+    plt.ylabel('Ratio')
+    plt.gca().invert_xaxis()
     plt.figure(3)
-    plt.plot(stages_list, vel_list, 'k-')
-    plt.xlabel('number of stages')
-    plt.ylabel('velocity in Km/s')
-    print("figure 3 is velocity (black) by stage")
+    plt.title("Velocity by Stage")
+    plt.plot(stages_list, vel_list, 'k-', label = "Velocity")
+    plt.legend(loc = 'upper left')
+    plt.xlabel('Stages left')
+    plt.ylabel('Velocity in Km/s')
+    plt.gca().invert_xaxis()
     plt.show()
 
 def check_answer(VEL_TARG, vel_burnout_total, calc_done):
     if vel_burnout_total >= VEL_TARG:
         vel_excess = vel_burnout_total - VEL_TARG
-        print("escaped sun with " + str(vel_excess) + " Km/s")
+        print("Escaped Sun with " + str(vel_excess) + " Km/s.")
     else:
         vel_short = VEL_TARG - vel_burnout_total
-        print("did not escape by " + str(vel_short) + " Km/s")
+        print("Did not escape by " + str(vel_short) + " Km/s.")
     calc_done = True
     return calc_done
 
 if __name__ == '__main__':
     while True:
-        print("currently only using engine velocities and ignoring specific impulse")
+        print("Currently only using engine velocities and ignoring specific impulse.")
         rocket_mass_total, fuel_total, eng_num, eng_vel = get_inputs()
         have_error = check_inputs(rocket_mass_total, fuel_total, have_error, PAYLOAD)
         if have_error == True:
             break
         vel_burnout_total, stages_list, current_list, next_list, E_k_list, P_k_list, vel_list = calculate(rocket_mass_total, fuel_total, eng_num, eng_vel, PAYLOAD, vel_burnout_total, stages_list, current_list, next_list, E_k_list, P_k_list, vel_list)
         calc_done = check_answer(VEL_TARG, vel_burnout_total, calc_done)
-        print("read graphs right to left and close all to finish program")
         plot_values(stages_list, current_list, next_list, E_k_list, P_k_list, vel_list, eng_num)
         if calc_done == True:
             break
